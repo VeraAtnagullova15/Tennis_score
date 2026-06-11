40c51637e514bd33544ea080597d8e1aab4c9058
Vera <verakrasnova56@gmail.com>
2026-06-11 09:20:25 -0400

    feat: add H2 sql-db imto project, add entity Player

---
+++ src/main/java/ru/atnagullova/tennis_score/entity/Player.java
@@ -0,0 +1,50 @@
+package ru.atnagullova.tennis_score.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+
